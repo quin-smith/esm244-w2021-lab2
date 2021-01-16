c97b70cf0897abd8f832889f82ab28d74bd3f306
quin-smith <quinlan_smith@yahoo.com>
2021-01-16 12:56:10 -0800

    added a shiny app

---
+++ penguins_shiny/app.R
@@ -0,0 +1,39 @@
+# Attache packages
+library(shiny)
+library(tidyverse)
+library(palmerpenguins)
+
+# Create user interface
+ui <- fluidPage(
+    titlePanel("I am adding a TITLE!"),
+    sidebarLayout(
+        sidebarPanel("put my widgets here!",
+                     radioButtons(inputId = "penguin_species",
+                                  label = "Choose penguin species",
+                                  choices = c("Adelie", "Chinny chin chins" = "Chinstrap", "Gentoo")
+                     )
+        ),
+        mainPanel("Here's my graph!",
+                  plotOutput(outputId = "penguin_plot"))
+    )
+    )
+
+# Create server function
+server <- function(input, output) {
+
+    penguin_select <- reactive({
+        penguins %>%
+            filter(species == input$penguin_species)
+    })
+
+    output$penguin_plot <- renderPlot({
+
+        ggplot(data = penguin_select(), aes(x = flipper_length_mm, y = body_mass_g)) +
+            geom_point()
+
+    })
+
+}
+
+# Combine into an app
+shinyApp(ui = ui, server = server)
